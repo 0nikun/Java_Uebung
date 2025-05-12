0a7edb6e48a501a3b81e9ea266bf4f281c8a9b30
0nikun <ino.woelke@gmail.com>
2025-05-12 11:01:31 +0200

    print gamefield

--- Java_Uebung/src/TicTacToeServer/TicTacToe.java
+++ Java_Uebung/src/TicTacToeServer/TicTacToe.java
@@ -2,4 +2,44 @@ package TicTacToeServer;
 
 public class TicTacToe {
 
+	public Boolean[][] gamefield = new Boolean[3][3];
+	
+	
+	public TicTacToe() {
+		printGamefield();
+	}
+	
+	
+	public void printGamefield() {
+		System.out.println("=========");
+		for(Boolean[] row : gamefield) {
+			System.out.print("| ");
+			for (Boolean field : row) {
+				char zeichen;
+				
+				if(field == null)
+					zeichen = ' ';
+				else if(field)
+					zeichen = 'X';
+				else
+					zeichen = 'O';
+				
+				System.out.print(zeichen +" ");
+			}
+			System.out.println("|");
+		}
+		System.out.println("=========");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		TicTacToe game1 = new TicTacToe();
+		game1.gamefield[0][0] = true;
+		game1.gamefield[1][1] = false;
+		game1.gamefield[2][2] = true;
+		
+		game1.printGamefield();
+		
+	}
 }
