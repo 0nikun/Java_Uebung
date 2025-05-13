b735ceeb48d90a05e767e401f599f8ee7a73e26f
0nikun <ino.woelke@gmail.com>
2025-05-13 10:39:26 +0200

    anfang WinCondition

--- Java_Uebung/src/TicTacToeServer/TicTacToe.java
+++ Java_Uebung/src/TicTacToeServer/TicTacToe.java
@@ -3,43 +3,61 @@ package TicTacToeServer;
 public class TicTacToe {
 
 	public Boolean[][] gamefield = new Boolean[3][3];
-	
-	
+
 	public TicTacToe() {
 		printGamefield();
 	}
-	
-	
+
 	public void printGamefield() {
 		System.out.println("=========");
-		for(Boolean[] row : gamefield) {
+		for (Boolean[] row : gamefield) {
 			System.out.print("| ");
 			for (Boolean field : row) {
 				char zeichen;
-				
-				if(field == null)
+
+				if (field == null)
 					zeichen = ' ';
-				else if(field)
+				else if (field)
 					zeichen = 'X';
 				else
 					zeichen = 'O';
-				
-				System.out.print(zeichen +" ");
+
+				System.out.print(zeichen + " ");
 			}
 			System.out.println("|");
 		}
 		System.out.println("=========");
 	}
-	
-	
+
+	public Boolean winCondition() {
+		for (Boolean[] row : gamefield) { // drei gleiche in einer Zeile
+			Boolean zeichen = row[0];
+			if (row[1] == zeichen == row[2])
+				return zeichen;
+		}
+		for (int spalte = 0; spalte < gamefield.length; spalte++) { // drei in einer Spalte
+			Boolean zeichen = gamefield[1][spalte];
+			if (gamefield[0][spalte] == zeichen == gamefield[2][spalte])
+				return zeichen;
+		}
+		// diagonal
+		if (gamefield[1][1] == gamefield[0][0] == gamefield[2][2])
+			return gamefield[1][1];
+
+		if (gamefield[1][1] == gamefield[0][2] == gamefield[2][0])
+			return gamefield[1][1];
+
+		return null;
+	}
+
 	public static void main(String[] args) {
-		
+
 		TicTacToe game1 = new TicTacToe();
 		game1.gamefield[0][0] = true;
 		game1.gamefield[1][1] = false;
 		game1.gamefield[2][2] = true;
-		
+
 		game1.printGamefield();
-		
+
 	}
 }
